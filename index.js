c86b4b13f3e48b60d052bd3b05286d6087402721
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-04-19 01:51:12 +0600

    delete requst functionality added

--- index.js
+++ index.js
@@ -90,6 +90,42 @@ async function run() {
       const result = await postCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
+    // update delete post
+    app.delete("/delete-post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postCollection.deleteOne(query);
+      res.send(result);
+    });
+    // get all the requests by email here
+    app.get("/requests/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await requestCollection.find(query).toArray();
+      res.send(result);
+    });
+    // delete request here 
+
+
+
+
+    app.delete("/delete-request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const filter = { _id: new ObjectId(id) };
+      const postId = req.query.postId;  
+      const postQuery = { _id: new ObjectId(postId) };
+      const updateDoc = {
+        $inc: {
+          request_count: -1
+        }
+      }
+      const updateRequestCount = await postCollection.updateOne(postQuery, updateDoc);
+      
+      const result = await requestCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
 
