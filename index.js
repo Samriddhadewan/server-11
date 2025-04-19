de93432f763e80ba49cd254a6aa3407adc5bc19c
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-04-19 20:49:54 +0600

    jwt token genarating and removing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.port || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5174", "http://localhost:5173"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phassignment.y94e1.mongodb.net/?appName=phAssignment`;
@@ -24,21 +32,47 @@ async function run() {
     const postCollection = db.collection("posts");
     const requestCollection = db.collection("requests");
 
+    // jwt token generator
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.JWT_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+    // clear cookie from browser
+    app.get("/logout", (req, res) => {
+      res.clearCookie("token", {
+        maxAge:0,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      }).send({ success: true });
+    });
 
-    // add volunteer need post here 
+    // add volunteer need post here
     app.post("/add-post", async (req, res) => {
       const jobData = req.body;
       const result = await postCollection.insertOne(jobData);
-      res.send(result)
-    });
-
-    // get all the posted jobs here 
-    app.get("/posts", async (req, res) => {
-      const result = await postCollection.find().toArray();
       res.send(result);
     });
 
-    // get a single post by id 
+    // get all the posted jobs here
+    app.get("/posts", async (req, res) => {
+      const search = req.query.search;
+      let query = {
+        title: { $regex: search, $options: "i" },
+      };
+      const result = await postCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get a single post by id
     app.get("/post/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -46,47 +80,51 @@ async function run() {
       res.send(result);
     });
 
-    // const add request here 
+    // const add request here
     app.post("/add-request", async (req, res) => {
       const requestData = req.body;
-      
-      const query = { email : requestData.email, postId: requestData.postId };
+
+      const query = { email: requestData.email, postId: requestData.postId };
       const existingRequest = await requestCollection.findOne(query);
-      if(existingRequest) {
-        return res.status(400).send({ message: "You have already applied for this campaign" });
+      if (existingRequest) {
+        return res
+          .status(400)
+          .send({ message: "You have already applied for this campaign" });
       }
       const result = await requestCollection.insertOne(requestData);
 
-      
-      const filter = {_id : new ObjectId(requestData.postId)};
+      const filter = { _id: new ObjectId(requestData.postId) };
       const updateDoc = {
-        $inc : {
-          request_count : 1}
-      }
+        $inc: {
+          request_count: 1,
+        },
+      };
 
-      const updateRequestCount = await postCollection.updateOne(filter, updateDoc);
-
+      const updateRequestCount = await postCollection.updateOne(
+        filter,
+        updateDoc
+      );
 
       res.send(result);
     });
 
-    // get my posted jobs here 
+    // get my posted jobs here
     app.get("/posts/:email", async (req, res) => {
       const email = req.params.email;
-      const query = { 'organizer.email': email };
+      const query = { "organizer.email": email };
       const result = await postCollection.find(query).toArray();
       res.send(result);
     });
 
-    // update a post here 
+    // update a post here
     app.put("/update-post/:id", async (req, res) => {
       const id = req.params.id;
       const updateData = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
-        $set: updateData
-      }
+        $set: updateData,
+      };
       const result = await postCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
@@ -104,31 +142,27 @@ async function run() {
       const result = await requestCollection.find(query).toArray();
       res.send(result);
     });
-    // delete request here 
-
-
-
+    // delete request here
 
     app.delete("/delete-request/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const filter = { _id: new ObjectId(id) };
-      const postId = req.query.postId;  
+      const postId = req.query.postId;
       const postQuery = { _id: new ObjectId(postId) };
       const updateDoc = {
         $inc: {
-          request_count: -1
-        }
-      }
-      const updateRequestCount = await postCollection.updateOne(postQuery, updateDoc);
-      
+          request_count: -1,
+        },
+      };
+      const updateRequestCount = await postCollection.updateOne(
+        postQuery,
+        updateDoc
+      );
+
       const result = await requestCollection.deleteOne(query);
       res.send(result);
     });
-
-
-
-
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
